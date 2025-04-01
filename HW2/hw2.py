bb1f6642115df276325e74bd82be67b3a0914ebb
Inbin Park <pinbin@naver.com>
2025-04-01 20:44:42 +0900

    hw2

---
+++ HW2/hw2.py
@@ -0,0 +1,12 @@
+a = 'My name is Inbin Park'
+print(len(a))
+print(a*10)         
+print(a[0])         
+print(a[:4])      
+print(a[-4:])       
+rev_a = a[::-1]     
+print(rev_a)       
+print(a[1:-1])
+print(a.upper())
+print(a.lower())
+print(a.replace('a', 'e'))
